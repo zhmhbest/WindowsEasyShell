dce56cdcda69856fda58dfe01be668ef0c10623b
zhmhbest <zhmhbest@gmail.com>
2021-01-20 19:52:16 +0800

    增强fieldping

--- bin/fieldping.sh
+++ bin/fieldping.sh
@@ -2,11 +2,15 @@
 # echo -n "Field($default_field): "
 # read field;
 # : ${field:=$default_field}
+
 if [ -z $1 ]; then
     field=192.168.1
+elif [[ "$1" =~ ^[0-9]+$ ]]; then
+    field=192.168.$1
 else
     field=$1
 fi
+
 echo "PING: $field.*"
 echo '----------------'
 if [ 'Windows_NT' == "$OS" ]; then
