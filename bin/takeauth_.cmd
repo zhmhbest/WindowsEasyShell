e6c119f145101733f9139a171231986fbd561cb1
zhmhbest <zhmhbest@gmail.com>
2022-06-13 23:39:26 +0800

    icals

--- bin/takeauth_.cmd
+++ bin/takeauth_.cmd
@@ -4,26 +4,19 @@
 @ECHO Work:[%CD%]
 @PAUSE
 
-:: TAKEOWN 管理员获得目录下所有文件的所有权
-::     /F <filename>   指定文件名或目录名模式
-::     /A              将所有权给于管理员组
-::     /R              递归（目录和子目录）
-::     /D Y            没有列出权限时自动获取
-:: @TAKEOWN /F .\* /A /R /D Y
-::
-:: ICACLS name [/grant[:r] Sid:perm[...]] [/remove[:g|:d]] Sid[...]] [/T] [/C] [/L] [/Q]
-::
-:: CACLS 修改文件的访问控制列表(ACL)
-::     /T              更改当前目录及其所有子目录中
-::     /G User:Perm    赋予指定用户访问权限。(Perm = R 读取 | W 写入 | C 写入 | F 完全控制)
-:: @ECHO Y|CACLS .\* /T /G %UserName%:F
-
-::操作当前目录
-@CALL :takeFile "%CD%"
-
-::操作子目录
-@FOR /F "usebackq delims=" %%f IN (`DIR /s /b "%CD%"`) DO @(
-    @CALL :takeFile "%%f"
+@IF "%~d0\" == "%CD%" (
+    @FOR /F "usebackq delims=" %%f IN (`DIR /a:-h-s /b "%CD%"`) DO @(
+        @ECHO [%%f]
+        @REM 当前用户为所有者
+        @ICACLS "%%f" /SetOwner %UserName% /T /C /L /Q >NUL && @ECHO Owner: √
+        @REM 使用继承的ACL（/Gant可删除此设置）
+        @ICACLS "%%f" /Reset /T /C /L /Q >NUL && @ECHO Reset: √
+    )
+) ELSE (
+    @REM 当前用户为所有者
+    @ICACLS "%CD%" /SetOwner %UserName% /T /C /L /Q >NUL && @ECHO Owner: √
+    @REM 使用继承的ACL（/Gant可删除此设置）
+    @ICACLS "%CD%" /Reset /T /C /L /Q >NUL && @ECHO Reset: √
 )
 
 @ECHO Done!
@@ -31,14 +24,24 @@
 @PAUSE
 @EXIT
 
-:takeFile
-@ECHO [%~1]
-@REM 所有权 交由 管理员
-@TAKEOWN /F "%~1" /A                    >NUL && ECHO OwnA  : √
-@REM 控制列表
-@ICACLS "%~1" /Grant Administrators:(F) >NUL && ECHO Admin : √
-@ICACLS "%~1" /Grant SYSTEM:(F)         >NUL && ECHO System: √
-@ICACLS "%~1" /Grant %UserName%:(F)     >NUL && ECHO User  : √
-@REM 所有权 交由 当前用户
-@ICACLS "%~1" /SetOwner %UserName%      >NUL && ECHO OwnU  : √
-@GOTO :EOF
+@REM ICACLS <filename> [options]
+@REM     /T     递归子目录中
+@REM     /C     错误继续
+@REM     /L     操作符号本身
+@REM     /Q     不显示成功消息
+@REM @ICACLS "%CD%" /Remove Administrators /T /C /Q
+@REM @ICACLS "%CD%" /SetOwner Administrators /T /C /Q
+@REM @ICACLS "%CD%" /Reset /T /C /Q
+@REM @ICACLS "%CD%" /Grant Administrators:(F)
+
+@REM TAKEOWN 管理员获得目录下所有文件的所有权
+@REM     /F <filename>   指定文件名或目录名模式
+@REM     /A              将所有权给于管理员组
+@REM     /R              递归（目录和子目录）
+@REM     /D Y            没有列出权限时自动获取
+@REM @TAKEOWN /F "%CD%" /A /R /D Y
+
+@REM CACLS 修改文件的访问控制列表(ACL)
+@REM     /T              更改当前目录及其所有子目录中
+@REM     /G User:Perm    赋予指定用户访问权限。(Perm = R 读取 | W 写入 | C 写入 | F 完全控制)
+@REM @ECHO Y|CACLS "%CD%" /T /G %UserName%:F
